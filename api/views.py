b09dec44196084efc99bf99987c27052119c28b9
khianvictorycalderon <ckhianvictory@gmail.com>
2026-03-10 18:46:28 +0800

    Added views (of serialized items)

--- api/views.py
+++ api/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import HttpResponse
+from rest_framework import viewsets
+from .models import Room, Boarder, Owner, House, Person, Item
+from .serializers import (
+    RoomSerializer,
+    BoarderSerializer,
+    OwnerSerializer,
+    HouseSerializer,
+    PersonSerializer,
+    ItemSerializer
+)
 
-# Create your views here.
-def demo(_):
-    return HttpResponse("API is working!")
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class BoarderViewSet(viewsets.ModelViewSet):
+    queryset = Boarder.objects.all()
+    serializer_class = BoarderSerializer
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+
+class HouseViewSet(viewsets.ModelViewSet):
+    queryset = House.objects.all()
+    serializer_class = HouseSerializer
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer

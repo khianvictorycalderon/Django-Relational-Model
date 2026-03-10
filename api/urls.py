a294134594c76b3fdc5f292ed736274fa725d2d2
khianvictorycalderon <ckhianvictory@gmail.com>
2026-03-10 18:45:28 +0800

    Added urls

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,23 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    RoomViewSet,
+    BoarderViewSet,
+    OwnerViewSet,
+    HouseViewSet,
+    PersonViewSet,
+    ItemViewSet
+)
+
+router = DefaultRouter()
+
+router.register(r'rooms', RoomViewSet)
+router.register(r'boarders', BoarderViewSet)
+router.register(r'owners', OwnerViewSet)
+router.register(r'houses', HouseViewSet)
+router.register(r'persons', PersonViewSet)
+router.register(r'items', ItemViewSet)
 
 urlpatterns = [
-    path("", views.demo)
+    path('', include(router.urls)),
 ]

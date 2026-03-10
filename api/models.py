9dbfbefa39eef04999f3d28689d9542c450a138c
khianvictorycalderon <ckhianvictory@gmail.com>
2026-03-10 18:42:18 +0800

    Added relational models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+# Django has 3 types of relationship, one-to-many, many-to-many, and one-to-one
+
+# --------------------------------------------------------------------
+# One to Many Relationship (One room can have multiple boarder)
+class Room(models.Model):
+    number = models.PositiveIntegerField()
+
+class Boarder(models.Model):
+    name = models.CharField(max_length=255)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="boarders")
+
+# --------------------------------------------------------------------
+# One to One Relationship (Only one owner per house)
+class Owner(models.Model):
+    name = models.CharField(max_length=255)
+
+class House(models.Model):
+    number = models.PositiveIntegerField()
+    owner = models.OneToOneField(Owner, on_delete=models.CASCADE, related_name="house")
+# --------------------------------------------------------------------
+# Many to Many Relationship (Many people can have many items)
+class Person(models.Model):
+    name = models.CharField(max_length=255)
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    buyer = models.ManyToManyField(Person, related_name="items")
+# --------------------------------------------------------------------
